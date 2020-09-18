f4387754d9512676c1b7ea247e56ecccac215c76
andres408 <2382@holbertonschool.com>
2020-09-18 19:39:39 +0000

    6.size correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
 * main - Prints size of some data types
 * description: Prints the sizes of a char, int, long int, and float
-* Return: 0 always
+* Return: 0
 */
 int main(void)
 {
@@ -12,10 +12,9 @@ long int longintType;
 long long int longlongType;
 float floatType;
 printf("Size of a char: %d byte(s)\n", sizeof(charType));
-printf("Size of a int: %d byte(s)\n", sizeof(intType));
+printf("Size of an int: %d byte(s)\n", sizeof(intType));
 printf("Size of a long int: %d byte(s)\n", sizeof(longintType));
 printf("Size of a long long int: %d byte(s)\n", sizeof(longlongType));
 printf("Size of a float: %d byte(s)\n", sizeof(floatType));
-
 return (0);
 }
