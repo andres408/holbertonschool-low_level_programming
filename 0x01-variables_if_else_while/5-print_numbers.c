d9841808d9cdd593356443daacf4f42ebb71b52b
andres408 <2382@holbertonschool.com>
2020-09-24 01:46:20 +0000

    Task 5 Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Display number 0-9
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+
+putchar (i);
+putchar ('\n');
+return (0);
+}
