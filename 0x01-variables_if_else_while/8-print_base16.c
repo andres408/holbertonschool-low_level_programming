4bf17ac33e0e5892b84d1b344e3beba5712b0fef
andres408 <2382@holbertonschool.com>
2020-09-24 02:29:10 +0000

    Task 8 hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - Display all hexadecimal numbers
+ *
+ * Return : Always 0 (Succes)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+
+putchar (i);
+for (i = 97; i <= 102; i++)
+
+putchar (i);
+putchar ('\n');
+return (0);
+}
