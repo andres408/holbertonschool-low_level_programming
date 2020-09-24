83ffc9bcbb34412fec00a7c4474bf6c51e836127
andres408 <2382@holbertonschool.com>
2020-09-24 02:01:39 +0000

    Task 7 smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Display alphabet in reverse
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+char i;
+for (i = 122; i >= 97; i--)
+
+putchar (i);
+putchar ('\n');
+return (0);
+}
