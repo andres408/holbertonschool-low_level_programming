e6027e73a42a4d7f302ffe1ac1d36f0d75087927
andres408 <2382@holbertonschool.com>
2020-09-28 01:13:11 +0000

    Task 10- 00..99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Print numbers 00..99
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+int h;
+for (i = 48; i <= 57; i++)
+{
+for (h = 48; h <=57; h++)
+{
+putchar (i);
+putchar (h);
+if (i == 57 && h == 57)
+continue;
+putchar (',');
+putchar (' ');
+}
+}
+putchar ('\n');
+return (0);
+}
