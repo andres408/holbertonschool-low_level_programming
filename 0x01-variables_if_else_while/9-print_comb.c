3234c5c91732027f5aaa25d9a3b44d25e0e0b9fc
andres408 <2382@holbertonschool.com>
2020-09-28 00:45:58 +0000

    task 9 correction

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Print all single digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+putchar (i);
+if (i == '9')
+continue;
+putchar (',');
+putchar (' ');
+}
+putchar ('\n');
+return (0);
+}
