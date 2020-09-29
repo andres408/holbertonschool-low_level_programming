1ab37e74641ab637278639e4954c5dd7e12cdbd2
andres408 <2382@holbertonschool.com>
2020-09-29 17:27:57 +0000

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include"holberton.h"
+/**
+ * print_rev - display string reverse
+ * @s: string pointer
+ */
+
+void print_rev(char *s)
+{
+int c;
+
+for (c = 0; s[c] != '\0'; c++)
+{
+;
+}
+for (c--; s[c] >= 0; c--)
+
+{
+_putchar(*(s + c));
+}
+_putchar('\n');
+}
