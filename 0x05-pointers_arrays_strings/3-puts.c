36470de791686372c70fc4a7aef13f75dd01a50c
andres408 <2382@holbertonschool.com>
2020-09-29 14:47:04 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include"holberton.h"
+/**
+ * _puts - print string
+ * @str: string pointer
+ */
+
+void _puts(char *str)
+{
+int c;
+for (c = 0; str[c] != '\0'; c++)
+
+{
+_putchar(*(str + c));
+}
+_putchar ('\n');
+}
