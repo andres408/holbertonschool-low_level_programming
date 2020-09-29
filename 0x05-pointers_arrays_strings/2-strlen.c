97186d357aaaea87ddb9a5f9eef156bdb417c756
andres408 <2382@holbertonschool.com>
2020-09-29 14:58:35 +0000

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include"holberton.h"
+/**
+ * _strlen - length of string
+ *
+ * @s: string pointer
+ * Return: int value
+ */
+int _strlen(char *s)
+{
+int c;
+for (c = 0; s[c] != '\0'; c++)
+{
+;
+}
+return (c);
+}
